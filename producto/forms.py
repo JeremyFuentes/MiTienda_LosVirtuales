a644f0eda3c47137fa22eb6e8da42c85d758251f
JeremyFuentes <fs8019032023@unab.edu.sv>
2023-11-17 20:59:02 -0600

    actualizacion 1 agregado de nuevas vistas

--- producto/forms.py
+++ producto/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Producto
+from .models import Producto, Contacto
 
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
+        fields = "__all__"
+        
+class ContactoForm(forms.ModelForm):
+    class Meta:
+        model = Contacto
         fields = "__all__"
--- producto/urls.py
+++ producto/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('productos/crear', views.crear_producto, name="crear_producto"),
     path('productos/editar/<int:id>', views.editar_producto, name="editar_producto"),
     path('productos/eliminar/<int:id>', views.eliminar_producto, name="eliminar_producto"),
+    path('AcercaDe', views.Acerca, name="AcercaDe" ),
+    path('Contacto', views.contacto, name="Contacto"),
 ]
